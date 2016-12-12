1c0c99823626a206adab9e48cf371a7b01bb0d96
HyotonStorm <jonathan.prost@epitech.eu>
2016-12-12 18:07:02 +0100

    mmmmh

--- Arbitre/Gomoku/Win.cpp
+++ Arbitre/Gomoku/Win.cpp
@@ -37,7 +37,6 @@ char	check(std::map<int, char> tab, int cc, int pos)
 				NT(tab, pos) == 0 && NT(tab, pos + 1) == 0 && NT(tab, pos - 3) == 0 && NT(tab, pos - 2) == 0 && NT(tab, pos - 1) == 0) ||
 			(pos % 19 < 19 && pos % 19 > 3 && tab[pos - 4] == tab[pos] && tab[pos - 3] == tab[pos] && tab[pos - 2] == tab[pos] && tab[pos - 1] == tab[pos] &&
 				NT(tab, pos) == 0 && NT(tab, pos - 4) == 0 && NT(tab, pos - 3) == 0 && NT(tab, pos - 2) == 0 && NT(tab, pos - 1) == 0) ||
-
 			(pos < 361 - 4 * 19 && tab[pos + 1 * 19] == tab[pos] && tab[pos + 2 * 19] == tab[pos] && tab[pos + 3 * 19] == tab[pos] && tab[pos + 4 * 19] == tab[pos] &&
 				NT(tab, pos) == 0 && NT(tab, pos + 19) == 0 && NT(tab, pos + 2 * 19) == 0 && NT(tab, pos + 3 * 19) == 0 && NT(tab, pos + 4 * 19) == 0) ||
 			(pos < 361 - 3 * 19 && tab[pos + 1 * 19] == tab[pos] && tab[pos + 2 * 19] == tab[pos] && tab[pos + 3 * 19] == tab[pos] && tab[pos - 1 * 19] == tab[pos] &&
@@ -48,7 +47,6 @@ char	check(std::map<int, char> tab, int cc, int pos)
 				NT(tab, pos) == 0 && NT(tab, pos + 19) == 0 && NT(tab, pos - 3 * 19) == 0 && NT(tab, pos - 2 * 19) == 0 && NT(tab, pos - 1 * 19) == 0) ||
 			(pos < 361 && tab[pos - 4 * 19] == tab[pos] && tab[pos - 3 * 19] == tab[pos] && tab[pos - 2 * 19] == tab[pos] && tab[pos - 1 * 19] == tab[pos] &&
 				NT(tab, pos) == 0 && NT(tab, pos - 4 * 19) == 0 && NT(tab, pos - 3 * 19) == 0 && NT(tab, pos - 2 * 19) == 0 && NT(tab, pos - 1 * 19) == 0) ||
-
 			(pos % 19 < 15 && pos < 361 - 4 * 19 && tab[pos + 1 * 19 + 1] == tab[pos] && tab[pos + 2 * 19 + 2] == tab[pos] && tab[pos + 3 * 19 + 3] == tab[pos] && tab[pos + 4 * 19 + 4] == tab[pos] &&
 				NT(tab, pos) == 0 && NT(tab, pos + 1 * 19 + 1) == 0 && NT(tab, pos + 2 * 19 + 2) == 0 && NT(tab, pos + 3 * 19 + 3) == 0 && NT(tab, pos + 4 * 19 + 4) == 0) ||
 			(pos % 19 < 16 && pos < 361 - 3 * 19 && tab[pos + 1 * 19 + 1] == tab[pos] && tab[pos + 2 * 19 + 2] == tab[pos] && tab[pos + 3 * 19 + 3] == tab[pos] && tab[pos - 1 * 19 - 1] == tab[pos] &&
@@ -59,18 +57,16 @@ char	check(std::map<int, char> tab, int cc, int pos)
 				NT(tab, pos) == 0 && NT(tab, pos + 1 * 19 + 1) == 0 && NT(tab, pos - 3 * 19 - 3) == 0 && NT(tab, pos - 2 * 19 - 2) == 0 && NT(tab, pos - 1 * 19 - 1) == 0) ||
 			(pos % 19 < 19 && pos < 361 && tab[pos - 4 * 19 - 4] == tab[pos] && tab[pos - 3 * 19 - 3] == tab[pos] && tab[pos - 2 * 19 - 2] == tab[pos] && tab[pos - 1 * 19 - 1] == tab[pos] &&
 				NT(tab, pos) == 0 && NT(tab, pos - 4 * 19 - 4) == 0 && NT(tab, pos - 3 * 19 - 3) == 0 && NT(tab, pos - 2 * 19 - 2) == 0 && NT(tab, pos - 1 * 19 - 1) == 0) ||
-
 			(pos % 19 > 3 && pos < 361 - 4 * 19 && tab[pos + 1 * 19 - 1] == tab[pos] && tab[pos + 2 * 19 - 2] == tab[pos] && tab[pos + 3 * 19 - 3] == tab[pos] && tab[pos + 4 * 19 - 4] == tab[pos] &&
-				NT(tab, pos) == 0 && NT(tab, pos + 1 * 19 - 1) == 0 && NT(tab, pos + 2 * 19 - 2) == 0 && NT(tab, pos + 3 * 19 - 3) == 0 && NT(tab, pos + 4 * 19 - 4) == 0)
+				NT(tab, pos) == 0 && NT(tab, pos + 1 * 19 - 1) == 0 && NT(tab, pos + 2 * 19 - 2) == 0 && NT(tab, pos + 3 * 19 - 3) == 0 && NT(tab, pos + 4 * 19 - 4) == 0) ||
 			(pos % 19 > 2 && pos < 361 - 3 * 19 && tab[pos + 1 * 19 - 1] == tab[pos] && tab[pos + 2 * 19 - 2] == tab[pos] && tab[pos + 3 * 19 - 3] == tab[pos] && tab[pos - 1 * 19 + 1] == tab[pos] &&
-				NT(tab, pos) == 0 && NT(tab, pos + 1 * 19 - 1) == 0 && NT(tab, pos + 2 * 19 - 2) == 0 && NT(tab, pos + 3 * 19 - 3) == 0 && NT(tab, pos - 1 * 19 + 1) == 0)
+				NT(tab, pos) == 0 && NT(tab, pos + 1 * 19 - 1) == 0 && NT(tab, pos + 2 * 19 - 2) == 0 && NT(tab, pos + 3 * 19 - 3) == 0 && NT(tab, pos - 1 * 19 + 1) == 0) ||
 			(pos % 19 > 1 && pos < 361 - 2 * 19 && tab[pos + 1 * 19 - 1] == tab[pos] && tab[pos + 2 * 19 - 2] == tab[pos] && tab[pos - 2 * 19 + 2] == tab[pos] && tab[pos - 1 * 19 + 1] == tab[pos] &&
-				NT(tab, pos) == 0 && NT(tab, pos + 1 * 19 - 1) == 0 && NT(tab, pos + 2 * 19 - 2) == 0 && NT(tab, pos - 2 * 19 + 2) == 0 && NT(tab, pos - 1 * 19 + 1) == 0)
+				NT(tab, pos) == 0 && NT(tab, pos + 1 * 19 - 1) == 0 && NT(tab, pos + 2 * 19 - 2) == 0 && NT(tab, pos - 2 * 19 + 2) == 0 && NT(tab, pos - 1 * 19 + 1) == 0) ||
 			(pos % 19 > 0 && pos < 361 - 1 * 19 && tab[pos + 1 * 19 - 1] == tab[pos] && tab[pos - 3 * 19 + 3] == tab[pos] && tab[pos - 2 * 19 + 2] == tab[pos] && tab[pos - 1 * 19 + 1] == tab[pos] &&
-				NT(tab, pos) == 0 && NT(tab, pos + 1 * 19 - 1) == 0 && NT(tab, pos - 3 * 19 + 3) == 0 && NT(tab, pos - 2 * 19 + 2) == 0 && NT(tab, pos - 1 * 19 + 1) == 0)
+				NT(tab, pos) == 0 && NT(tab, pos + 1 * 19 - 1) == 0 && NT(tab, pos - 3 * 19 + 3) == 0 && NT(tab, pos - 2 * 19 + 2) == 0 && NT(tab, pos - 1 * 19 + 1) == 0) ||
 			(pos < 361 && tab[pos - 4 * 19 + 4] == tab[pos] && tab[pos - 3 * 19 + 3] == tab[pos] && tab[pos - 2 * 19 + 2] == tab[pos] && tab[pos - 1 * 19 + 1] == tab[pos] &&
 				NT(tab, pos) == 0 && NT(tab, pos - 4 * 19 + 4) == 0 && NT(tab, pos - 3 * 19 + 3) == 0 && NT(tab, pos - 2 * 19 + 2) == 0 && NT(tab, pos - 1 * 19 + 1) == 0))
-
 			return tab[pos];
 	}
 	else if ((pos % 19 < 15 && tab[pos + 1] == tab[pos] && tab[pos + 2] == tab[pos] && tab[pos + 3] == tab[pos] && tab[pos + 4] == tab[pos]) ||
@@ -78,19 +74,16 @@ char	check(std::map<int, char> tab, int cc, int pos)
 		(pos % 19 < 17 && pos % 19 > 1 && tab[pos + 1] == tab[pos] && tab[pos + 2] == tab[pos] && tab[pos - 2] == tab[pos] && tab[pos - 1] == tab[pos]) ||
 		(pos % 19 < 18 && pos % 19 > 2 && tab[pos + 1] == tab[pos] && tab[pos - 3] == tab[pos] && tab[pos - 2] == tab[pos] && tab[pos - 1] == tab[pos]) ||
 		(pos % 19 < 19 && pos % 19 > 3 && tab[pos - 4] == tab[pos] && tab[pos - 3] == tab[pos] && tab[pos - 2] == tab[pos] && tab[pos - 1] == tab[pos]) ||
-
 		(pos < 361 - 4 * 19 && tab[pos + 1 * 19] == tab[pos] && tab[pos + 2 * 19] == tab[pos] && tab[pos + 3 * 19] == tab[pos] && tab[pos + 4 * 19] == tab[pos]) ||
 		(pos < 361 - 3 * 19 && tab[pos + 1 * 19] == tab[pos] && tab[pos + 2 * 19] == tab[pos] && tab[pos + 3 * 19] == tab[pos] && tab[pos - 1 * 19] == tab[pos]) ||
 		(pos < 361 - 2 * 19 && tab[pos + 1 * 19] == tab[pos] && tab[pos + 2 * 19] == tab[pos] && tab[pos - 2 * 19] == tab[pos] && tab[pos - 1 * 19] == tab[pos]) ||
 		(pos < 361 - 1 * 19 && tab[pos + 1 * 19] == tab[pos] && tab[pos - 3 * 19] == tab[pos] && tab[pos - 2 * 19] == tab[pos] && tab[pos - 1 * 19] == tab[pos]) ||
 		(pos < 361 && tab[pos - 4 * 19] == tab[pos] && tab[pos - 3 * 19] == tab[pos] && tab[pos - 2 * 19] == tab[pos] && tab[pos - 1 * 19] == tab[pos]) ||
-
 		(pos % 19 < 15 && pos < 361 - 4 * 19 && tab[pos + 1 * 19 + 1] == tab[pos] && tab[pos + 2 * 19 + 2] == tab[pos] && tab[pos + 3 * 19 + 3] == tab[pos] && tab[pos + 4 * 19 + 4] == tab[pos]) ||
 		(pos % 19 < 16 && pos < 361 - 3 * 19 && tab[pos + 1 * 19 + 1] == tab[pos] && tab[pos + 2 * 19 + 2] == tab[pos] && tab[pos + 3 * 19 + 3] == tab[pos] && tab[pos - 1 * 19 - 1] == tab[pos]) ||
 		(pos % 19 < 17 && pos < 361 - 2 * 19 && tab[pos + 1 * 19 + 1] == tab[pos] && tab[pos + 2 * 19 + 2] == tab[pos] && tab[pos - 2 * 19 - 2] == tab[pos] && tab[pos - 1 * 19 - 1] == tab[pos]) ||
 		(pos % 19 < 18 && pos < 361 - 1 * 19 && tab[pos + 1 * 19 + 1] == tab[pos] && tab[pos - 3 * 19 - 3] == tab[pos] && tab[pos - 2 * 19 - 2] == tab[pos] && tab[pos - 1 * 19 - 1] == tab[pos]) ||
 		(pos % 19 < 19 && pos < 361 && tab[pos - 4 * 19 - 4] == tab[pos] && tab[pos - 3 * 19 - 3] == tab[pos] && tab[pos - 2 * 19 - 2] == tab[pos] && tab[pos - 1 * 19 - 1] == tab[pos]) ||
-
 		(pos % 19 > 3 && pos < 361 - 4 * 19 && tab[pos + 1 * 19 - 1] == tab[pos] && tab[pos + 2 * 19 - 2] == tab[pos] && tab[pos + 3 * 19 - 3] == tab[pos] && tab[pos + 4 * 19 - 4] == tab[pos]) ||
 		(pos % 19 > 2 && pos < 361 - 3 * 19 && tab[pos + 1 * 19 - 1] == tab[pos] && tab[pos + 2 * 19 - 2] == tab[pos] && tab[pos + 3 * 19 - 3] == tab[pos] && tab[pos - 1 * 19 + 1] == tab[pos]) ||
 		(pos % 19 > 1 && pos < 361 - 2 * 19 && tab[pos + 1 * 19 - 1] == tab[pos] && tab[pos + 2 * 19 - 2] == tab[pos] && tab[pos - 2 * 19 + 2] == tab[pos] && tab[pos - 1 * 19 + 1] == tab[pos]) ||
