d3da669592ef47db16ed2d71ac1985dd467f37cd
Robin Mouret <robin.mouret@epitech.eu>
2016-12-14 00:54:46 +0100

    squlq

--- Arbitre/Gomoku/Double_Trois.cpp
+++ Arbitre/Gomoku/Double_Trois.cpp
@@ -3,258 +3,299 @@
 
 int		alligne_trois(std::map<int, char> tab, int pos, int *pos2, int *pos3)
 {
-	int	nb = 0;
+	int		nb = 0;
+	char	e;
 
-	if (tab[pos] == tab[pos + 1] && tab[pos] == tab[pos + 2] &&
-		tab[pos + 3] == '-' && tab[pos - 1] == '-')
+	if (tab[pos] == 'x')
+		e = 'o';
+	else (tab[pos] == 'o')
+		e = 'x';
+	if (pos % 19 < 16 && pos % 19 > 0 && 
+		tab[pos] == tab[pos + 1] && tab[pos] == tab[pos + 2] &&
+		tab[pos + 3] != e && tab[pos - 1] != e)
 	{
 		*pos2 = pos + 1;
 		*pos3 = pos + 2;
 		nb++;
 	}
-	if (tab[pos] == tab[pos + 1] && tab[pos] == tab[pos + 3] &&
-		tab[pos + 2] == '-' && tab[pos - 1] == '-')
+	if (pos % 19 < 16 && pos % 19 > 0 &&
+		tab[pos] == tab[pos + 1] && tab[pos] == tab[pos + 3] &&
+		tab[pos + 2] != e && tab[pos - 1] != e)
 	{
 		*pos2 = pos + 1;
 		*pos3 = pos + 3;
 		nb++;
 	}
-	if (tab[pos] == tab[pos + 2] && tab[pos] == tab[pos + 3] &&
-		tab[pos + 1] == '-' && tab[pos - 1] == '-')
+	if (pos % 19 < 16 && pos % 19 > 0 &&
+		tab[pos] == tab[pos + 2] && tab[pos] == tab[pos + 3] &&
+		tab[pos + 1] != e && tab[pos - 1] != e)
 	{
 		*pos2 = pos + 2;
 		*pos3 = pos + 3;
 		nb++;
 	}
-	if (tab[pos] == tab[pos - 1] && tab[pos] == tab[pos + 2] &&
-		tab[pos + 1] == '-' && tab[pos - 2] == '-')
+	if (pos % 19 < 17 && pos % 19 > 1 &&
+		tab[pos] == tab[pos - 1] && tab[pos] == tab[pos + 2] &&
+		tab[pos + 1] != e && tab[pos - 2] != e)
 	{
 		*pos2 = pos - 1;
 		*pos3 = pos + 2;
 		nb++;
 	}
-	if (tab[pos] == tab[pos + 1] && tab[pos] == tab[pos - 1] &&
-		tab[pos + 2] == '-' && tab[pos - 2] == '-')
+	if (pos % 19 < 17 && pos % 19 > 1 &&
+		tab[pos] == tab[pos + 1] && tab[pos] == tab[pos - 1] &&
+		tab[pos + 2] != e && tab[pos - 2] != e)
 	{
 		*pos2 = pos - 1;
 		*pos3 = pos + 1;
 		nb++;
 	}
-	if (tab[pos] == tab[pos - 2] && tab[pos] == tab[pos + 1] &&
-		tab[pos - 1] == '-' && tab[pos + 2] == '-')
+	if (pos % 19 < 17 && pos % 19 > 1 &&
+		tab[pos] == tab[pos - 2] && tab[pos] == tab[pos + 1] &&
+		tab[pos - 1] != e && tab[pos + 2] != e)
 	{
 		*pos2 = pos + 1;
 		*pos3 = pos - 2;
 		nb++;
 	}
-	if (tab[pos] == tab[pos - 2] && tab[pos] == tab[pos - 3] &&
-		tab[pos + 1] == '-' && tab[pos - 1] == '-')
+	if (pos % 19 < 18 && pos % 19 > 2 &&
+		tab[pos] == tab[pos - 2] && tab[pos] == tab[pos - 3] &&
+		tab[pos + 1] != e && tab[pos - 1] != e)
 	{
 		*pos2 = pos - 2;
 		*pos3 = pos - 3;
 		nb++;
 	}
-	if (tab[pos] == tab[pos - 1] && tab[pos] == tab[pos - 3] &&
-		tab[pos - 2] == '-' && tab[pos + 1] == '-')
+	if (pos % 19 < 18 && pos % 19 > 2 &&
+		tab[pos] == tab[pos - 1] && tab[pos] == tab[pos - 3] &&
+		tab[pos - 2] != e && tab[pos + 1] != e)
 	{
 		*pos2 = pos - 1;
 		*pos3 = pos - 3;
 		nb++;
 	}
-	if (tab[pos] == tab[pos - 1] && tab[pos] == tab[pos - 2] &&
-		tab[pos - 3] == '-' && tab[pos + 1] == '-')
+	if (pos % 19 < 18 && pos % 19 > 2 &&
+		tab[pos] == tab[pos - 1] && tab[pos] == tab[pos - 2] &&
+		tab[pos - 3] != e && tab[pos + 1] != e)
 	{
 		*pos2 = pos - 1;
 		*pos3 = pos - 2;
 		nb++;
 	}
 
-	if (tab[pos] == tab[pos + 19] && tab[pos] == tab[pos + 2 * 19] &&
-		tab[pos + 3 * 19] == '-' && tab[pos - 19] == '-')
+	if (pos < 303 && pos > 18 &&
+		tab[pos] == tab[pos + 19] && tab[pos] == tab[pos + 2 * 19] &&
+		tab[pos + 3 * 19] != e && tab[pos - 19] != e)
 	{
 		*pos2 = pos + 19;
 		*pos3 = pos + 2 * 19;
 		nb++;
 	}
-	if (tab[pos] == tab[pos + 19] && tab[pos] == tab[pos + 3 * 19] &&
-		tab[pos + 2 * 19] == '-' && tab[pos - 19] == '-')
+	if (pos < 303 && pos > 18 &&
+		tab[pos] == tab[pos + 19] && tab[pos] == tab[pos + 3 * 19] &&
+		tab[pos + 2 * 19] != e && tab[pos - 19] != e)
 	{
 		*pos2 = pos + 19;
 		*pos3 = pos + 3 * 19;
 		nb++;
 	}
-	if (tab[pos] == tab[pos + 2 * 19] && tab[pos] == tab[pos + 3 * 19] &&
-		tab[pos + 19] == '-' && tab[pos - 19] == '-')
+	if (pos < 303 && pos > 18 &&
+		tab[pos] == tab[pos + 2 * 19] && tab[pos] == tab[pos + 3 * 19] &&
+		tab[pos + 19] != e && tab[pos - 19] != e)
 	{
 		*pos2 = pos + 2 * 19;
 		*pos3 = pos + 3 * 19;
 		nb++;
 	}
-	if (tab[pos] == tab[pos - 19 * 2] && tab[pos] == tab[pos + 19] &&
-		tab[pos + 2 * 19] == '-' && tab[pos - 2 * 19] == '-')
+	if (pos < 322 && pos > 37 &&
+		tab[pos] == tab[pos - 19 * 2] && tab[pos] == tab[pos + 19] &&
+		tab[pos + 2 * 19] != e && tab[pos - 2 * 19] != e)
 	{
 		*pos2 = pos - 19 * 2;
 		*pos3 = pos + 19;
 		nb++;
 	}
-	if (tab[pos] == tab[pos - 19] && tab[pos] == tab[pos + 19] &&
-		tab[pos + 2 * 19] == '-' && tab[pos - 2 * 19] == '-')
+	if (pos < 322 && pos > 37 &&
+		tab[pos] == tab[pos - 19] && tab[pos] == tab[pos + 19] &&
+		tab[pos + 2 * 19] != e && tab[pos - 2 * 19] != e)
 	{
 		*pos2 = pos - 19;
 		*pos3 = pos + 19;
 		nb++;
 	}
-	if (tab[pos] == tab[pos + 19 * 2] && tab[pos] == tab[pos - 19] &&
-		tab[pos + 2 * 19] == '-' && tab[pos - 2 * 19] == '-')
+	if (pos < 322 && pos > 37 &&
+		tab[pos] == tab[pos + 19 * 2] && tab[pos] == tab[pos - 19] &&
+		tab[pos + 2 * 19] != e && tab[pos - 2 * 19] != e)
 	{
 		*pos2 = pos + 19 * 2;
 		*pos3 = pos - 19;
 		nb++;
 	}
-	if (tab[pos] == tab[pos - 2 * 19] && tab[pos] == tab[pos - 3 * 19] &&
-		tab[pos + 19] == '-' && tab[pos - 19] == '-')
+	if (pos < 341 && pos > 56 &&
+		tab[pos] == tab[pos - 2 * 19] && tab[pos] == tab[pos - 3 * 19] &&
+		tab[pos + 19] != e && tab[pos - 19] != e)
 	{
 		*pos2 = pos - 2 * 19;
 		*pos3 = pos - 3 * 19;
 		nb++;
 	}
-	if (tab[pos] == tab[pos - 19] && tab[pos] == tab[pos - 3 * 19] &&
-		tab[pos - 2 * 19] == '-' && tab[pos + 19] == '-')
+	if (pos < 341 && pos > 56 &&
+		tab[pos] == tab[pos - 19] && tab[pos] == tab[pos - 3 * 19] &&
+		tab[pos - 2 * 19] != e && tab[pos + 19] != e)
 	{
 		*pos2 = pos - 19;
 		*pos3 = pos - 3 * 19;
 		nb++;
 	}
-	if (tab[pos] == tab[pos - 19] && tab[pos] == tab[pos - 2 * 19] &&
-		tab[pos - 3 * 19] == '-' && tab[pos + 19] == '-')
+	if (pos < 341 && pos > 56 &&
+		tab[pos] == tab[pos - 19] && tab[pos] == tab[pos - 2 * 19] &&
+		tab[pos - 3 * 19] != e && tab[pos + 19] != e)
 	{
 		*pos2 = pos - 19;
 		*pos3 = pos - 2 * 19;
 		nb++;
 	}
 
-	if (tab[pos] == tab[pos + 1 + 19] && tab[pos] == tab[pos + 2 + 19 * 2] &&
-		tab[pos + 3 + 19 * 3] == '-' && tab[pos - 1 - 19] == '-')
+	if (pos % 19 < 16 && pos % 19 > 0 && pos < 303 && pos > 18 &&
+		tab[pos] == tab[pos + 1 + 19] && tab[pos] == tab[pos + 2 + 19 * 2] &&
+		tab[pos + 3 + 19 * 3] != e && tab[pos - 1 - 19] != e)
 	{
 		*pos2 = pos + 1 + 19;
 		*pos3 = pos + 2 + 19 * 2;
 		nb++;
 	}
-	if (tab[pos] == tab[pos + 1 + 19] && tab[pos] == tab[pos + 3 + 19 * 3] &&
-		tab[pos + 2 + 19 * 2] == '-' && tab[pos - 1 - 19] == '-')
+	if (pos % 19 < 16 && pos % 19 > 0 && pos < 303 && pos > 18 &&
+		tab[pos] == tab[pos + 1 + 19] && tab[pos] == tab[pos + 3 + 19 * 3] &&
+		tab[pos + 2 + 19 * 2] != e && tab[pos - 1 - 19] != e)
 	{
 		*pos2 = pos + 1 + 19;
 		*pos3 = pos + 3 + 19 * 3;
 		nb++;
 	}
-	if (tab[pos] == tab[pos + 2 + 2 * 19] && tab[pos] == tab[pos + 3 + 19 * 3] &&
-		tab[pos + 1 + 19] == '-' && tab[pos - 1 - 19] == '-')
+	if (pos % 19 < 16 && pos % 19 > 0 && pos < 303 && pos > 18 &&
+		tab[pos] == tab[pos + 2 + 2 * 19] && tab[pos] == tab[pos + 3 + 19 * 3] &&
+		tab[pos + 1 + 19] != e && tab[pos - 1 - 19] != e)
 	{
 		*pos2 = pos + 2 + 2 * 19;
 		*pos3 = pos + 3 + 3 * 19;
 		nb++;
 	}
-	if (tab[pos] == tab[pos + 1 + 19] && tab[pos] == tab[pos - 2 - 19 * 2] &&
-		tab[pos + 2 + 19 * 2] == '-' && tab[pos - 1 - 19] == '-')
+	if (pos % 19 < 17 && pos % 19 > 1 && pos < 322 && pos > 37 &&
+		tab[pos] == tab[pos + 1 + 19] && tab[pos] == tab[pos - 2 - 19 * 2] &&
+		tab[pos + 2 + 19 * 2] != e && tab[pos - 1 - 19] != e)
 	{
 		*pos2 = pos - 2 - 2 * 19;
 		*pos3 = pos + 1 + 19;
 		nb++;
 	}
-	if (tab[pos] == tab[pos + 1 + 19] && tab[pos] == tab[pos - 19 - 1] &&
-		tab[pos + 2 + 19 * 2] == '-' && tab[pos - 2 - 19 * 2] == '-')
+	if (pos % 19 < 17 && pos % 19 > 1 && pos < 322 && pos > 37 &&
+		tab[pos] == tab[pos + 1 + 19] && tab[pos] == tab[pos - 19 - 1] &&
+		tab[pos + 2 + 19 * 2] != e && tab[pos - 2 - 19 * 2] != e)
 	{
 		*pos2 = pos - 1 - 19;
 		*pos3 = pos + 1 + 19;
 		nb++;
 	}
-	if (tab[pos] == tab[pos - 1 - 19] && tab[pos] == tab[pos + 2 + 19 * 2] &&
-		tab[pos - 2 - 19 * 2] == '-' && tab[pos + 1 + 19] == '-')
+	if (pos % 19 < 17 && pos % 19 > 1 && pos < 322 && pos > 37 &&
+		tab[pos] == tab[pos - 1 - 19] && tab[pos] == tab[pos + 2 + 19 * 2] &&
+		tab[pos - 2 - 19 * 2] != e && tab[pos + 1 + 19] != e)
 	{
 		*pos2 = pos + 2 + 2 * 19;
 		*pos3 = pos - 1 - 19;
 		nb++;
 	}
-	if (tab[pos] == tab[pos - 2 - 2 * 19] && tab[pos] == tab[pos - 3 - 19 * 3] &&
-		tab[pos + 1 + 19] == '-' && tab[pos - 1 - 19] == '-')
+	if (pos % 19 < 18 && pos % 19 > 2 && pos < 341 && pos > 56 &&
+		tab[pos] == tab[pos - 2 - 2 * 19] && tab[pos] == tab[pos - 3 - 19 * 3] &&
+		tab[pos + 1 + 19] != e && tab[pos - 1 - 19] != e)
 	{
 		*pos2 = pos - 2 - 2 * 19;
 		*pos3 = pos - 3 - 3 * 19;
 		nb++;
 	}
-	if (tab[pos] == tab[pos - 1 - 19] && tab[pos] == tab[pos - 3 - 19 * 3] &&
-		tab[pos - 2 - 19 * 2] == '-' && tab[pos + 1 + 19] == '-')
+	if (pos % 19 < 18 && pos % 19 > 2 && pos < 341 && pos > 56 &&
+		tab[pos] == tab[pos - 1 - 19] && tab[pos] == tab[pos - 3 - 19 * 3] &&
+		tab[pos - 2 - 19 * 2] != e && tab[pos + 1 + 19] != e)
 	{
 		*pos2 = pos - 1 - 19;
 		*pos3 = pos - 3 - 19 * 3;
 		nb++;
 	}
-	if (tab[pos] == tab[pos - 1 - 19] && tab[pos] == tab[pos - 2 - 19 * 2] &&
-		tab[pos + 1 + 19] == '-' && tab[pos - 3 - 19 * 3] == '-')
+	if (pos % 19 < 18 && pos % 19 > 2 && pos < 341 && pos > 56 &&
+		tab[pos] == tab[pos - 1 - 19] && tab[pos] == tab[pos - 2 - 19 * 2] &&
+		tab[pos + 1 + 19] != e && tab[pos - 3 - 19 * 3] != e)
 	{
 		*pos2 = pos - 1 - 19;
 		*pos3 = pos - 2 - 2 * 19;
 		nb++;
 	}
 
-	if (tab[pos] == tab[pos - 1 + 19] && tab[pos] == tab[pos - 2 + 2 * 19] &&
-		tab[pos - 3 + 19 * 3] == '-' && tab[pos + 1 - 19] == '-')
+	if (pos % 19 < 18 && pos % 19 > 2 && pos < 303 && pos > 18 &&
+		tab[pos] == tab[pos - 1 + 19] && tab[pos] == tab[pos - 2 + 2 * 19] &&
+		tab[pos - 3 + 19 * 3] != e && tab[pos + 1 - 19] != e)
 	{
 		*pos2 = pos - 1 + 19;
 		*pos3 = pos - 2 + 19 * 2;
 		nb++;
 	}
-	if (tab[pos] == tab[pos - 1 + 19] && tab[pos] == tab[pos - 3 + 3 * 19] &&
-		tab[pos - 2 + 19 * 2] == '-' && tab[pos + 1 - 19] == '-')
+	if (pos % 19 < 18 && pos % 19 > 2 && pos < 303 && pos > 18 &&
+		tab[pos] == tab[pos - 1 + 19] && tab[pos] == tab[pos - 3 + 3 * 19] &&
+		tab[pos - 2 + 19 * 2] != e && tab[pos + 1 - 19] != e)
 	{
 		*pos2 = pos - 1 + 19;
 		*pos3 = pos - 3 + 19 * 3;
 		nb++;
 	}
-	if (tab[pos] == tab[pos - 2 + 2 * 19] && tab[pos] == tab[pos - 3 + 3 * 19] &&
-		tab[pos - 1 + 19] == '-' && tab[pos + 1 - 19] == '-')
+	if (pos % 19 < 18 && pos % 19 > 2 && pos < 303 && pos > 18 &&
+		tab[pos] == tab[pos - 2 + 2 * 19] && tab[pos] == tab[pos - 3 + 3 * 19] &&
+		tab[pos - 1 + 19] != e && tab[pos + 1 - 19] != e)
 	{
 		*pos2 = pos - 2 + 19 * 2;
 		*pos3 = pos - 3 + 19 * 3;
 		nb++;
 	}
-	if (tab[pos] == tab[pos - 1 + 19] && tab[pos] == tab[pos + 2 - 19 * 2] &&
-		tab[pos + 1 - 19] == '-' && tab[pos - 2 + 19 * 2] == '-')
+	if (pos % 19 < 17 && pos % 19 > 1 && pos < 322 && pos > 37 &&
+		tab[pos] == tab[pos - 1 + 19] && tab[pos] == tab[pos + 2 - 19 * 2] &&
+		tab[pos + 1 - 19] != e && tab[pos - 2 + 19 * 2] != e)
 	{
 		*pos2 = pos - 1 + 19;
 		*pos3 = pos + 2 - 19 * 2;
 		nb++;
 	}
-	if (tab[pos] == tab[pos - 1 + 19] && tab[pos] == tab[pos + 1 - 19] &&
-		tab[pos + 2 - 19 * 2] == '-' && tab[pos - 2 + 19 * 2] == '-')
+	if (pos % 19 < 17 && pos % 19 > 1 && pos < 322 && pos > 37 &&
+		tab[pos] == tab[pos - 1 + 19] && tab[pos] == tab[pos + 1 - 19] &&
+		tab[pos + 2 - 19 * 2] != e && tab[pos - 2 + 19 * 2] != e)
 	{
 		*pos2 = pos - 1 + 19;
 		*pos3 = pos + 1 - 19;
 		nb++;
 	}
-	if (tab[pos] == tab[pos + 1 - 19] && tab[pos] == tab[pos - 2 + 19 * 2] &&
-		tab[pos - 1 + 19] == '-' && tab[pos + 2 - 19 * 2] == '-')
+	if (pos % 19 < 17 && pos % 19 > 1 && pos < 322 && pos > 37 &&
+		tab[pos] == tab[pos + 1 - 19] && tab[pos] == tab[pos - 2 + 19 * 2] &&
+		tab[pos - 1 + 19] != e && tab[pos + 2 - 19 * 2] != e)
 	{
 		*pos2 = pos + 1 - 19;
 		*pos3 = pos - 2 + 19 * 2;
 		nb++;
 	}
-	if (tab[pos] == tab[pos + 2 - 2 * 19] && tab[pos] == tab[pos + 3 - 3 * 19] &&
-		tab[pos - 1 + 19] == '-' && tab[pos + 1 - 19] == '-')
+	if (pos % 19 < 16 && pos % 19 > 0 && pos < 341 && pos > 56 &&
+		tab[pos] == tab[pos + 2 - 2 * 19] && tab[pos] == tab[pos + 3 - 3 * 19] &&
+		tab[pos - 1 + 19] != e && tab[pos + 1 - 19] != e)
 	{
 		*pos2 = pos + 2 - 19 * 2;
 		*pos3 = pos + 3 - 19 * 3;
 		nb++;
 	}
-	if (tab[pos] == tab[pos + 1 - 19] && tab[pos] == tab[pos + 3 - 3 * 19] &&
-		tab[pos + 2 - 19 * 2] == '-' && tab[pos - 1 + 19] == '-')
+	if (pos % 19 < 16 && pos % 19 > 0 && pos < 341 && pos > 56 &&
+		tab[pos] == tab[pos + 1 - 19] && tab[pos] == tab[pos + 3 - 3 * 19] &&
+		tab[pos + 2 - 19 * 2] != e && tab[pos - 1 + 19] != e)
 	{
 		*pos2 = pos + 1 - 19;
 		*pos3 = pos + 3 - 19 * 3;
 		nb++;
 	}
-	if (tab[pos] == tab[pos + 1 - 19] && tab[pos] == tab[pos + 2 - 2 * 19] &&
-		tab[pos + 3 - 19 * 3] == '-' && tab[pos - 1 + 19] == '-')
+	if (pos % 19 < 16 && pos % 19 > 0 && pos < 341 && pos > 56 &&
+		tab[pos] == tab[pos + 1 - 19] && tab[pos] == tab[pos + 2 - 2 * 19] &&
+		tab[pos + 3 - 19 * 3] != e && tab[pos - 1 + 19] != e)
 	{
 		*pos2 = pos + 1 - 19;
 		*pos3 = pos + 2 - 19 * 2;
