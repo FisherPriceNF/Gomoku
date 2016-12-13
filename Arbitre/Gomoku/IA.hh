e55c86fb55c92bc4dc15ce623a4378f6a906103e
mouret_b <robin.mouret@epitech.eu>
2016-12-13 15:51:06 +0100

    push

--- Arbitre/Gomoku/IA.hh
+++ Arbitre/Gomoku/IA.hh
@@ -4,30 +4,30 @@
 
 #include "Gomoku.h"
 
-class IA
+class	IA
 {
 public:
-	IA() {};
-	IA(int, int, char, char);
-	~IA() {};
+  IA() {};
+  IA(int, int, char, char);
+  ~IA() {};
 
-	void Play(std::map<int, char>*, int*, int*);
+  void	Play(std::map<int, char>*, int*, int*);
 	
-	struct Chosen
-	{
-		signed int	weight, p;
-	};
+  struct	Chosen
+  {
+    signed int	weight, p;
+  };
 
 private:
-	int		eval(std::map<int, char>*, int, int);
-	Chosen	min(std::map<int, char>, int, int, Chosen, Chosen);
-	Chosen	max(std::map<int, char>, int, int, Chosen, Chosen);
-	Chosen	newChosen(int, int);
-	Chosen	MinChosen();
-	Chosen	MaxChosen();
+  int		eval(std::map<int, char>*, int, int);
+  Chosen	min(std::map<int, char>, int, int, Chosen, Chosen);
+  Chosen	max(std::map<int, char>, int, int, Chosen, Chosen);
+  Chosen	newChosen(int, int);
+  Chosen	MinChosen();
+  Chosen	MaxChosen();
 
-	char	c, e;
-	int		cc, db;
+  char		c, e;
+  int		cc, db;
 };
 
 #endif // !IA_HH_
